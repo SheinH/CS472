44d6bc6690da39a941b510d9974fd9a3a581759d
rockyrockz95 <62509419+rockyrockz95@users.noreply.github.com>
2024-03-12 15:11:25 -0400

    Weird Triangles

--- hw2/vshader2a.glsl
+++ hw2/vshader2a.glsl
@@ -1,11 +1,19 @@
 #version 330
 
-in	vec2	a_Position;	// attribute variable
+in	vec2	a_Position;	// attribute variable: position vector
+in	vec3	a_Color;	// attribute variable: color vector
+
+out	vec4	v_Color;	// varying variable for passing color to fragment shader
+
+uniform	mat4	u_Modelview;	// uniform variable for passing modelview  matrix
 uniform	mat4	u_Projection;	// uniform variable for passing projection matrix
+uniform	float	u_Theta;	// Theta parameter
+uniform	int	u_Twist;	// Twist flag
 
 void main() 
 {
 	// PUT YOUR CODE HERE
-        gl_Position = u_Projection * vec4(a_Position, 0, 1);
-        gl_PointSize = 4;
+	gl_Position = u_Projection * u_Modelview * vec4(a_Position, 0, 1);
+	v_Color = vec4(a_Color, 1);
+	gl_PointSize = 4;
 }
