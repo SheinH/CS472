5881e914042fc0bc539314652d2b4a63c89b6eb8
Joquanna <62509419+rockyrockz95@users.noreply.github.com>
2024-04-09 15:50:47 -0400

    Update fshader3b1.glsl

--- hw3/fshader3b1.glsl
+++ hw3/fshader3b1.glsl
@@ -1,8 +1,14 @@
 #version 330
 
 // PUT YOUR CODE HERE
+in vec2 v_TexCoord;
+
+uniform	sampler2D u_Sampler;	// uniform variable for the texture image
+
+out vec4 fragColor;
 
 void main() 
 {
 	// PUT YOUR CODE HERE
+	fragColor = texture(u_Sampler, v_TexCoord);
 }
