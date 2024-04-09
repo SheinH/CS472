38a51f784a79b0325cdb2faa1a5c0a82fe80a532
Joquanna <62509419+rockyrockz95@users.noreply.github.com>
2024-04-09 15:49:04 -0400

    Update fshader3b2.glsl

--- hw3/fshader3b2.glsl
+++ hw3/fshader3b2.glsl
@@ -1,24 +1,10 @@
 #version 330
 
 // PUT YOUR CODE HERE
-// input vertex data
-in vec3  a_Position;
-in vec2  a_TexCoord;
-in vec3  a_Color;
-in vec3  a_Normal;
-
-// output data; will be interpolated for each fragment.
-out vec2 v_TexCoord;
-
-// Values that stay constant for the whole mesh.
-uniform mat4 u_Projection;
-uniform mat4 u_View;
-uniform vec3 u_LightDirection;
+out vec4 fragColor;
 
 void main() 
 {
 	// PUT YOUR CODE HERE
-	// Output position of the vertex, in clip space : MVP * position
-	gl_Position =  u_Projection * u_View * vec4(a_Position, 1);
-		
+	fragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
