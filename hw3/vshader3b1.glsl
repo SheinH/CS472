fcdc1f49b59756a4f4573e8e0fbf7d9e76e07230
Joquanna <62509419+rockyrockz95@users.noreply.github.com>
2024-04-09 15:50:05 -0400

    Update vshader3b1.glsl

--- hw3/vshader3b1.glsl
+++ hw3/vshader3b1.glsl
@@ -1,8 +1,19 @@
 #version 330
 
 // PUT YOUR CODE HERE
+in vec3  a_Position;
+in vec2  a_TexCoord;
+in vec3  a_Color;
+in vec3  a_Normal;
+
+out vec2 v_TexCoord;
+
+uniform mat4 u_Projection;
+uniform mat4 u_View;
+uniform vec3 u_LightDirection;
 
 void main() 
 {
-	// PUT YOUR CODE HERE
+    gl_Position =  u_Projection * u_View * vec4(a_Position, 1);
+  	v_TexCoord = a_TexCoord;
 }
